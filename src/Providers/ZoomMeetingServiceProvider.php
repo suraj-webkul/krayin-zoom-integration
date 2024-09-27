e42bdea563c6a72a47de31550ebdc119ac59615f
suraj kashyap <suraj.kashyap370@webkul.in>
2024-09-27 12:38:18 +0530

    fix publish able issue.

--- src/Providers/ZoomMeetingServiceProvider.php
+++ src/Providers/ZoomMeetingServiceProvider.php
@@ -25,8 +25,8 @@ class ZoomMeetingServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../../publishable/assets'                                            => public_path('zoom'),
-            __DIR__.'/../Resources/views/components/activities/actions/activity.blade.php' => __DIR__.'/../../../Admin/src/Resources/views/components/activities/actions/activity.blade.php',
-            __DIR__.'/../Resources/views/activities/edit.blade.php'                        => __DIR__.'/../../../Admin/src/Resources/views/activities/edit.blade.php',
+            __DIR__.'/../Resources/views/components/activities/actions/activity.blade.php' => base_path().'/packages/Webkul/Admin/src/Resources/views/components/activities/actions/activity.blade.php',
+            __DIR__.'/../Resources/views/activities/edit.blade.php'                        => base_path().'/packages/Webkul/Admin/src/Resources/views/activities/edit.blade.php',
         ], 'public');
 
         Blade::anonymousComponentPath(__DIR__.'/../Resources/views/components', 'zoom_meeting');
@@ -42,6 +42,8 @@ class ZoomMeetingServiceProvider extends ServiceProvider
         Event::listen('admin.activities.edit.form_controls.after', function ($viewRenderEventManager) {
             $viewRenderEventManager->addTemplate('zoom_meeting::activities.zoom');
         });
+
+        $this->app->register(ModuleServiceProvider::class);
     }
 
     /**
